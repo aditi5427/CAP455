bf975f3cdfee3906f6418a3f9dfa55cd104f4d53
Aditi Suman <98769992+aditi5427@users.noreply.github.com>
2024-08-24 12:02:32 +0530

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int r, a;
+    cout << "Enter the radius " << endl;
+    cin >> r;
+
+    a = 3.14 * r * r;
+
+    cout << "Area of the circle" << a << endl;
+
+    return 0;
+}
