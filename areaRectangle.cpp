fafa65cf3b1581070cf9badfcb983aa7031a12b6
Aditi Suman <98769992+aditi5427@users.noreply.github.com>
2024-08-25 12:27:21 +0530

    Add files via upload

---
+++ areaRectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int l , b , a;
+    cout << "Enter length and breadth" << endl;
+    cin >> l >> b;
+    a = l * b;
+
+    cout << "Area of recatngle is: " << a << endl;
+
+    return 0;
+}
