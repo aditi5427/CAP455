defae04cc4fe94f1d426093d12519b96ac58f041
Aditi Suman <98769992+aditi5427@users.noreply.github.com>
2024-08-25 12:27:47 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int l,  a;
+    cout <<"Enter length " << endl;
+    cin >> l;
+
+    a = l*l;
+    cout << "Area of the square is: " << a << endl;
+
+    return 0;
+}
