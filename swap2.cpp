02d80f90921bd489ef5d379129dfe9cdd92da835
Aditi Suman <98769992+aditi5427@users.noreply.github.com>
2024-08-26 15:22:01 +0530

    Swap in C++

---
+++ swap2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, a, y;
+    // clrscr();
+
+    cout << "Enter two values" << endl;
+    cin >> x >> y ;
+    x= a;
+    y = x;
+    a = y;
+    cout << x << y << endl;
+
+    return 0;
+}
